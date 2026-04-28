90acd8d609174f06f7823081b00d88f2f05dfa31
zubarsolomia3-cmyk <zubarsolomia3@gmail.com>
2026-04-28 22:21:44 +0300

    task 3 done

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", event => {
+  const inputValue = event.currentTarget.value.trim();
+
+  nameOutput.textContent = inputValue || "Anonymous";
+});
