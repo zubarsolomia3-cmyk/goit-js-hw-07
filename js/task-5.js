4f7b592cbe254224f8e95fb069fe07000c926420
zubarsolomia3-cmyk <zubarsolomia3@gmail.com>
2026-04-28 22:35:09 +0300

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
