c4f1af82004226e9817612231fd19044534c490f
zubarsolomia3-cmyk <zubarsolomia3@gmail.com>
2026-04-28 22:27:52 +0300

    task 4 done

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+});
